1bf7c557f2d2ed3d2eb61808fe36f87bc8136fcf
Luis Medina <luismedina.git@outlook.com>
2020-04-10 11:24:30 -0400

    Sidebar content flex

--- app/frontend/src/components/TheSideBar.vue
+++ app/frontend/src/components/TheSideBar.vue
@@ -1,14 +1,29 @@
 <template>
   <aside class="the-sidebar">
-    ROUTES
+    <div class="the-sidebar__header">
+      ROUTES
+    </div>
+    <div class="the-sidebar__content">
+      asd
+    </div>
   </aside>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .the-sidebar {
+  display: flex;
   height: 100%;
   width: 300px;
   padding: 10px;
   color: #FFF;
+  flex-direction: column;
+
+  &__header {
+
+  }
+
+  &__content {
+    flex-grow: 1;
+  }
 }
 </style>
